062078c22da5aa124de514fd2ea3824f7a13e78d
Wesley Alinyo <wesleyalinyo@gmail.com>
2024-09-22 15:55:50 +0300

    adds file

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import time
+
+def countdown(n):
+    while n > 0:
+        print(f'{n} SECOND(S)!')
+        n -= 1
+    print("HAPPY NEW YEAR!")
+
+def countdown_with_sleep(n):
+    while n > 0:
+        print(f'{n} SECOND(S)!')
+        time.sleep(1)  # Pause for one second
+        n -= 1
+    print("HAPPY NEW YEAR!")
+
+# Example:
+countdown(10)
+countdown_with_sleep(10)  #Runs with a one-second pause
+
